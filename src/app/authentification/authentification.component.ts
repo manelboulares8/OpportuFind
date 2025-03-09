f5b1fe7087b269dd0c38765c53d348bb6789755b
compte stagiaire <stagiaire@neopolis-dev.com>
2025-03-09 14:01:33 +0100

    first

---
+++ src/app/authentification/authentification.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-authentification',
+  standalone: false,
+  templateUrl: './authentification.component.html',
+  styleUrl: './authentification.component.css'
+})
+export class AuthentificationComponent {
+  userType: string = ''; // Variable for user type selection
+
+
+
+
+}
