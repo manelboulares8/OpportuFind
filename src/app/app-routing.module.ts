89cc2e44bae961b7c76764c73b973eeef7c200b7
compte stagiaire <stagiaire@neopolis-dev.com>
2025-03-25 15:50:27 +0100

    radithom components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CategoryComponent } from './category/category.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorComponent } from './error/error.component';
+import { JobDetailComponent } from './job-detail/job-detail.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 const routes: Routes = [
   { path: 'authentification', component: AuthentificationComponent }, 
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Page d'accueil par défaut
   { path: 'home', component: HomeComponent }, // Ajoute cette ligne pour que 'home' fonctionne
-
+  {path: 'about',component:AboutComponent},
+  {path : 'category',component:CategoryComponent},
+  {path :'contact',component:ContactComponent},
+  {path :'error',component:ErrorComponent},
+  {path :'job-detail',component:JobDetailComponent},
+  {path:'job-list',component:JobListComponent},
+  {path:'testimonial',component:TestimonialComponent},
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastModule } from 'primeng/toast';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'; // Importer HttpClientModule
-import { AuthService } from './services/auth.service'; // Assure-toi que ton AuthService est bien importé
+import { AuthService } from './services/auth.service';
+import { AboutComponent } from './about/about.component';
+import { CategoryComponent } from './category/category.component';
+import { ContactComponent } from './contact/contact.component';
+import { JobDetailComponent } from './job-detail/job-detail.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ErrorComponent } from './error/error.component'; // Assure-toi que ton AuthService est bien importé
+import { MessageService } from 'primeng/api';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthentificationComponent
+    AuthentificationComponent,
+    AboutComponent,
+    CategoryComponent,
+    ContactComponent,
+    JobDetailComponent,
+    JobListComponent,
+    TestimonialComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     FormsModule,
     HttpClientModule, // Ajouter HttpClientModule ici
     ReactiveFormsModule,  // Ajouter ReactiveFormsModule dans les imports
+    ToastModule,
+      // Assure-toi que RouterModule est ici
 
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
